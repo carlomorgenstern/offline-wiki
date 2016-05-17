c300f712d7bb0dfb753f1b06e5db63703e117a7e
Carlo Morgenstern <carlomorgenstern@gmail.com>
2016-05-17 02:29:15 +0200

    final working commit; uglifying js with es6 is still broken, so no uglify

--- src/js/app.js
+++ src/js/app.js
@@ -1,50 +1,183 @@
-/*jslint browser: true*/
-/*global $*/
+/* jslint browser: true */
+/* global $, Materialize */
 'use strict';
-$(document).ready(function() {
+$(document).ready(() => {
 	if ('serviceWorker' in navigator) {
-		navigator.serviceWorker.register('/serviceworker.js').then(function(registration) {
-			console.log('ServiceWorker registration successful with scope: ', registration.scope);
-		}).catch(function(err) {
-			console.log('ServiceWorker registration failed: ', err);
+		navigator.serviceWorker.register('/serviceworker.js').then(() => {
+			if (!navigator.serviceWorker.controller) {
+				location.reload();
+			}
+		}).catch(error => {
+			$('#errorTitle').text('ServiceWorker-Registrierung fehlgeschlagen');
+			$('#errorDescription').text('Bitte laden Sie die Seite neu und kontaktieren Sie bei wiederholter Fehlermeldung den Ersteller der Seite.');
+			$('#errorCard').removeClass('hide');
+			$('#storeOffline').prop('disabled', true);
+			console.log('ServiceWorker registration failed: ', error);
 		});
 	} else {
-		// ServiceWorker not available
+		$('#errorTitle').text('ServiceWorker nicht vorhanden');
+		$('#errorDescription').html($.parseHTML('Ihr Browser implementiert den ServiceWorker nicht, womit Artikel nicht offline gespeichert werden können. ' +
+			'Bitte besuchen Sie die Seite mit einem Browser, der ServiceWorker implementiert. <a href="https://jakearchibald.github.io/isserviceworkerready/">(IsServiceWorkerReady?)</a>'));
+		$('#errorCard').removeClass('hide');
+		$('#storeOffline').prop('disabled', true);
 	}
 
-	// fires when the search input field changes
-	$('#search').change($('#search').val(), function(searchString) {
-		return new promise(function(resolve, reject) {
-            $.ajax({
-                url: "https://de.wikipedia.org/w/api.php",
-                jsonp: "callback",
-                dataType: 'jsonp',
-                data: {
-                    action: "query",
-                    titles: searchString,
-                    format: "json",
-                    redirects: "true",
-                    prop: 'extracts|revisions',
-                    explaintext: 1,
-                    exsentences: 1
-                }
-            }).done(function(data) {
-                console.log(data.query.pages);
-                /*var page = data.query.pages[Object.keys(data.query.pages)[0]];
+	$('#search').change(() => {
+		queryApi($('#search').val()).then(pageTitle => {
+			window.location.hash = pageTitle;
+		});
+	});
 
-                if ('missing' in page) {
-                    reject("not found");
-                } else {
-                    $("#name").text(page.title);
-                    $("#zuletzt").text(page.revisions[0].timestamp);
-                    resolve({
-                        title: page.title.replace(/\s/g, '_'),
-                        pageid: page.pageid
-                    });
-                }*/
-            }).fail(function(error) {
-                reject(error);
-            });
-        });
-	})
+	$('#clearSearch').click(() => {
+		$('#search').val('');
+		window.location.hash = '';
+	});
+
+	$('#storeOffline').change(() => {
+		if ($('#storeOffline').prop('checked') === true) {
+			$('#storeOffline').prop('disabled', true);
+			sendMessage('addToCache', $('#trueTitle').text()).catch(error => {
+				console.log('Error while adding page to cache: ', error);
+			}).then(() => $('#storeOffline').prop('disabled', false));
+		} else {
+			$('#storeOffline').prop('disabled', true);
+			sendMessage('deleteFromCache', $('#trueTitle').text()).catch(error => {
+				console.log('Error while deleting page from cache: ', error);
+			}).then(() => $('#storeOffline').prop('disabled', false));
+		}
+	});
+
+	$(window).on('hashchange', () => {
+		if (window.location.hash === '') {
+			$('#progressBar').removeClass('hide');
+			$('#title').text('Offline Wikipedia');
+			$('#trueTitle').text('Offline Wikipedia');
+			$('#storeOffline').prop('checked', false);
+			$('#storeOffline').prop('disabled', true);
+			$('#wikiContainer').load('nosearch.html #container', () => {
+				sendMessage('listArticles').then(articles => {
+					if (articles.length !== 0) {
+						$('#offlineArticles').html('');
+					}
+					articles.forEach(article => {
+						$('#offlineArticles').append('<a href="#' + article.title + '" class="collection-item">' + article.title + '</a>');
+					});
+				}).then(() => $('#progressBar').addClass('hide'));
+			});
+		} else {
+			$('#progressBar').removeClass('hide');
+			parsePage(window.location.hash.substring(1))
+				.then(displayPage)
+				.catch(displayError)
+				.then(() => $('#progressBar').addClass('hide'));
+		}
+	}).trigger('hashchange');
+
+	function queryApi(searchString) {
+		return new Promise((resolve, reject) => {
+			$.ajax({
+				url: 'https://de.wikipedia.org/w/api.php',
+				dataType: 'jsonp',
+				cache: 'true',
+				jsonp: 'callback',
+				jsonpCallback: 'wikiQueryCallback',
+				data: {
+					action: 'query',
+					titles: searchString,
+					format: 'json',
+					redirects: 'true',
+					prop: 'extracts|revisions',
+					explaintext: 1,
+					exsentences: 1
+				}
+			}).done(data => {
+				let page = data.query.pages[Object.keys(data.query.pages)[0]];
+				if ('missing' in page) {
+					reject('no page found');
+				} else {
+					resolve(page.title.replace(/\s/g, '_'));
+				}
+			}).fail(error => {
+				reject(error);
+			});
+		});
+	}
+
+	function parsePage(pageTitle) {
+		return new Promise((resolve, reject) => {
+			$.ajax({
+				url: 'https://de.wikipedia.org/w/api.php',
+				dataType: 'jsonp',
+				cache: 'true',
+				jsonp: 'callback',
+				jsonpCallback: 'wikiParseCallback',
+				data: {
+					action: 'parse',
+					format: 'json',
+					page: pageTitle,
+					prop: 'text',
+					redirects: 'true',
+					disablelimitreport: 'true',
+					disableeditsection: 'true',
+					disabletoc: 'true'
+				}
+			}).done(data => {
+				if ('error' in data) {
+					reject('no page found');
+				} else {
+					resolve({
+						title: pageTitle,
+						pageHtml: data.parse.text[Object.keys(data.parse.text)[0]]
+					});
+				}
+			}).fail(error => {
+				reject(error);
+			});
+		});
+	}
+
+	function displayPage(page) {
+		sendMessage('isInCache', page.title).then(result => {
+			if (result) {
+				$('#storeOffline').prop('checked', true);
+			} else {
+				$('#storeOffline').prop('checked', false);
+			}
+			$('#storeOffline').prop('disabled', false);
+		});
+		$('#title').text(page.title.replace(/_/g, ' '));
+		$('#search').val(page.title.replace(/_/g, ' '));
+		$('#trueTitle').text(page.title);
+		$('#wikiContainer').html(page.pageHtml);
+		$('#wikiContainer a[href^="#"]').attr('href', '');
+		$('#wikiContainer a[href^="/wiki/"]').attr('href', function() {
+			return decodeURIComponent(this.href.split('#')[0].replace('/wiki/', '#'));
+		});
+	}
+
+	function displayError(error) {
+		if (error === 'no page found') {
+			Materialize.toast('Es wurde kein Wikipedia-Artikel für ihre Suche gefunden.', 2000);
+		} else {
+			Materialize.toast('Fehler beim Herunterladen des Artikels. Bitte probieren Sie es erneut.', 4000);
+			console.log('Error while downloading article: ', error);
+		}
+	}
+
+	function sendMessage(command, identifier) {
+		return new Promise(function(resolve, reject) {
+			let messageChannel = new MessageChannel();
+			messageChannel.port1.onmessage = event => {
+				if (event.data.error) {
+					reject(event.data.error);
+				} else {
+					resolve(event.data);
+				}
+			};
+			navigator.serviceWorker.controller.postMessage({
+				command: command,
+				identifier: identifier
+			}, [messageChannel.port2]);
+		});
+	}
 });
--- src/serviceworker.js
+++ src/serviceworker.js
@@ -1,66 +1,144 @@
-/*jslint browser: true*/
-/*global self, caches*/
+/* jslint worker:true */
+/* global caches */
 'use strict';
-var CACHE_NAME = 'offline-wiki-cache-v1';
-var urlsToCache = [
-	'/',
-	'/css/styles.css',
-	'/fonts/MaterialIcons-Regular.woff2',
-	'/fonts/roboto/Roboto-Bold.woff2',
-	'/fonts/roboto/Roboto-Light.woff2',
-	'/fonts/roboto/Roboto-Medium.woff2',
-	'/fonts/roboto/Roboto-Regular.woff2',
-	'/fonts/roboto/Roboto-Thin.woff2',
-	'/js/bundle.js'
-];
+var CACHE_VERSION = '1';
+var CACHE_NAME = 'offline-wiki-cache-v' + CACHE_VERSION;
+var tmpCache = {};
 
-// Set the callback for the install step
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
 	event.waitUntil(
-		caches.open(CACHE_NAME)
-		.then(function(cache) {
-			console.log('Opened cache');
-			return cache.addAll(urlsToCache);
+		caches.open(CACHE_NAME).then(cache => {
+			return cache.addAll([
+				'/index.html',
+				'/nosearch.html',
+				'/css/styles.css',
+				'/fonts/MaterialIcons-Regular.woff2',
+				'/fonts/roboto/Roboto-Bold.woff2',
+				'/fonts/roboto/Roboto-Light.woff2',
+				'/fonts/roboto/Roboto-Medium.woff2',
+				'/fonts/roboto/Roboto-Regular.woff2',
+				'/fonts/roboto/Roboto-Thin.woff2',
+				'/js/bundle.js',
+				'https://de.wikipedia.org/w/load.php?debug=false&lang=de&modules=site&only=styles&skin=vector'
+			]);
 		})
 	);
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        console.log("Catched event - logging request object");
-        console.log(event.request);
-        return fetch(event.request);
-        /*if (response) {
-          return response;
-        }
-
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have 2 stream.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );*/
-      }
-    )
-  );
+self.addEventListener('activate', event => {
+	let cacheWhitelist = [CACHE_NAME];
+	event.waitUntil(
+		caches.keys().then(cacheNames => {
+			return Promise.all(
+				cacheNames.map(cacheName => {
+					if (cacheWhitelist.indexOf(cacheName) === -1) {
+						return caches.delete(cacheName);
+					}
+				})
+			);
+		})
+	);
 });
+
+self.addEventListener('fetch', event => {
+	let requestUrl = new URL(event.request.url);
+	event.respondWith(
+		caches.match(event.request).then(matchResponse => {
+			if (matchResponse) {
+				return matchResponse;
+			}
+			return fetch(event.request.clone()).then(response => {
+				if (requestUrl.hostname === 'de.wikipedia.org' && requestUrl.pathname === '/w/api.php' && requestUrl.search.includes('action=parse') && requestUrl.search.includes('page')) {
+					tmpCache[extractParameterValueFromUrl(requestUrl, 'page')] = {
+						request: event.request,
+						response: response.clone()
+					};
+				}
+				return response;
+			});
+		})
+	);
+});
+
+self.addEventListener('message', event => {
+	caches.open(CACHE_NAME).then(cache => {
+		let identifier = encodeURIComponent(event.data.identifier);
+		if (event.data.command === 'addToCache') {
+			if (identifier in tmpCache) {
+				cache.put(tmpCache[identifier].request.clone(), tmpCache[identifier].response.clone())
+					.then(() => {
+						event.ports[0].postMessage({
+							action: 'added',
+							identifier: identifier,
+							url: tmpCache[identifier].request.url
+						});
+					});
+			} else {
+				throw 'Requested page is not in temporary cache: ' + identifier;
+			}
+		} else if (event.data.command === 'deleteFromCache') {
+			cache.keys().then(requests => {
+				requests.some(request => {
+					if (extractParameterValueFromUrl(new URL(request.url), 'page') === identifier) {
+						cache.delete(request).then(() => {
+							event.ports[0].postMessage({
+								action: 'deleted',
+								identifier: identifier,
+								url: request.url
+							});
+							return true;
+						});
+					}
+				});
+			});
+		} else if (event.data.command === 'isInCache') {
+			let result = false;
+			cache.keys().then(requests => {
+				result = requests.some(request => {
+					if (extractParameterValueFromUrl(new URL(request.url), 'page') === identifier) {
+						return true;
+					}
+				});
+			}).then(() => {
+				event.ports[0].postMessage(result);
+			});
+		} else if (event.data.command === 'listArticles') {
+			let articles = [];
+			cache.keys().then(requests => {
+				requests.forEach(request => {
+					let requestUrl = new URL(request.url);
+					if (requestUrl.search.includes('action=parse')) {
+						articles.push({
+							title: decodeURIComponent(extractParameterValueFromUrl(requestUrl, 'page'))
+						});
+					}
+				});
+			}).then(() => {
+				event.ports[0].postMessage(articles);
+			});
+		} else {
+			throw 'Unknown command: ' + event.data.command;
+		}
+	}).catch(error => {
+		event.ports[0].postMessage({
+			error: error
+		});
+	});
+});
+
+function extractParameterValueFromUrl(url, parameter) {
+	if ('search' in url && typeof url.search == 'string') {
+		let parameterArray = url.search.substring(1).split('&');
+		let parameterValue = false;
+		parameterArray.some((parameterString) => {
+			if (parameterString.indexOf(parameter) === 0) {
+				parameterValue = parameterString.split("=")[1];
+				return true;
+			}
+		});
+		if (parameterValue !== false) {
+			return parameterValue;
+		}
+	}
+	return false;
+}
